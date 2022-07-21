e626d841d2b6f570e2372fb059fda6afab9eaa6f
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-07-21 11:43:09 +0300

    addTodoFormState

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -1,13 +1,38 @@
-import { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import styles from './AddTodoForm.module.scss'
 
 const AddTodoForm = (): ReactElement => {
+  const [checkedTodo, setCheckedTodo] = useState<boolean>(false)
+  const [userTodo, setUserTodo] = useState<string>('')
+  const [userId, setUserId] = useState<number>(0)
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedTodo(!checkedTodo)
+  }
+
+  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log('clicked', e)
+    console.log('clicked', e.target)
+  }
+
   return (
     <form onSubmit={(e) => e.preventDefault()} className={styles.form}>
-      <input className={styles.inputUserTodo} type={'text'} placeholder='User todo' />
-      <input type={'number'} placeholder='UserId' />
-      <input className={styles.inputUserCompleted} type={'checkbox'} />
-      <button type={'submit'}>Add new todo</button>
+      <input
+        className={styles.inputUserTodo}
+        type={'text'}
+        placeholder='User todo'
+        defaultValue={userTodo}
+      />
+      <input type={'number'} placeholder='UserId' defaultValue={userId} />
+      <input
+        className={styles.inputUserCompleted}
+        type={'checkbox'}
+        onChange={onChangeHandler}
+        defaultChecked={checkedTodo}
+      />
+      <button type={'submit'} onClick={onClickHandler}>
+        Add new todo
+      </button>
     </form>
   )
 }
