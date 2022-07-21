918cdeda6dc68241a4ab3de2d3fc18101acd708a
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-07-21 17:03:48 +0300

    notes

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -1,36 +1,58 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useId, useState } from 'react'
+import { Todo } from '../../constants/TYPES'
 import styles from './AddTodoForm.module.scss'
 
 const AddTodoForm = (): ReactElement => {
   const [checkedTodo, setCheckedTodo] = useState<boolean>(false)
   const [userTodo, setUserTodo] = useState<string>('')
   const [userId, setUserId] = useState<number>(0)
+  const [todo, setTodo] = useState<Todo>({
+    id: Date.now(),
+    userId: userId,
+    title: userTodo,
+    completed: checkedTodo,
+  })
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCheckedTodo(!checkedTodo)
   }
 
-  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const addHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log('clicked', e)
     console.log('clicked', e.target)
   }
 
+  const onTextChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserTodo(e.target.value)
+  }
+  const onNumberChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(Number(e.target.value))
+  }
   return (
     <form onSubmit={(e) => e.preventDefault()} className={styles.form}>
       <input
         className={styles.inputUserTodo}
         type={'text'}
-        placeholder='User todo'
+        placeholder='Write the description of todo'
         defaultValue={userTodo}
+        value={userTodo}
+        onChange={onTextChangeHandler}
+        id='text'
       />
-      <input type={'number'} placeholder='UserId' defaultValue={userId} />
+      <input
+        type={'number'}
+        placeholder='UserId'
+        defaultValue={userId}
+        value={userId}
+        onChange={onNumberChangeHandler}
+      />
       <input
         className={styles.inputUserCompleted}
         type={'checkbox'}
         onChange={onChangeHandler}
         defaultChecked={checkedTodo}
       />
-      <button type={'submit'} onClick={onClickHandler}>
+      <button type={'submit'} onClick={addHandler}>
         Add new todo
       </button>
     </form>
