b4e9d8df6ab6d960a8c3a8eb3172fa19fc73b174
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-01 17:43:28 +0300

    added btn for mint

--- src/Pages/Mint/Mint.tsx
+++ src/Pages/Mint/Mint.tsx
@@ -1,9 +1,12 @@
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import styles from './Mint.module.scss'
 
 const Mint = (): ReactElement => {
+  const btnHandler = (e: React.MouseEvent) => {
+    console.log('clicked mint btn')
+  }
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -11,7 +14,12 @@ const Mint = (): ReactElement => {
       </div>
       <div className={styles.main}>
         <div className={styles.content}>
-          <div className={styles.content__wrapeer}>Minting</div>
+          <div className={styles.content__wrapeer}>
+            <p>Go mint</p>
+            <button onClick={btnHandler} className={styles.btnMinting}>
+              Mint
+            </button>
+          </div>
         </div>
       </div>
       <div className={styles.footer}>
