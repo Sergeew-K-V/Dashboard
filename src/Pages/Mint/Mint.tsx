94177c31ee03cdd85dab7d3e26ab2814e9cf558a
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-02 15:01:43 +0300

    need connect web3 and front

--- src/Pages/Mint/Mint.tsx
+++ src/Pages/Mint/Mint.tsx
@@ -1,14 +1,28 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import styles from './Mint.module.scss'
 import Web3 from 'web3'
 
 const Mint = (): ReactElement => {
-  var web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'))
+  // var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://mainnet.infura.io/ws'))
+
+  var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:3000'))
+
   const btnHandler = (e: React.MouseEvent) => {
     console.log('clicked mint btn')
   }
+  const connectHandler = async (e: React.MouseEvent) => {}
+  const [connection, setConnection] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      alert('Install metamask!')
+    }
+  }, [])
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -18,8 +32,12 @@ const Mint = (): ReactElement => {
         <div className={styles.content}>
           <div className={styles.content__wrapeer}>
             <p>Go mint</p>
+            {connection === true ? <p>Address:</p> : <p>Not connected</p>}
             <button onClick={btnHandler} className={styles.btnMinting}>
               Mint
+            </button>
+            <button onClick={connectHandler} className={styles.btnMinting}>
+              Connect Wallet
             </button>
           </div>
         </div>
---
+++ src/contracts/OwlMint.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4 < 0.9.0;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract Azuki is ERC721A {
+    constructor() ERC721A("Azuki", "AZUKI") {}
+
+    function mint(uint256 quantity) external payable {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+}
+
